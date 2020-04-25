5c871b1c43f0a629c1c030c82341517976eb6345
tarpaha <ppopchikovsky@gmail.com>
2020-04-26 00:51:53 +0300

    bash version of process.cmd

---
+++ process.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+country=Russia
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv --output-document=global.csv
+python3 filter_country.py global.csv $country local.csv
+python3 draw_graph.py local.csv $country local.png
